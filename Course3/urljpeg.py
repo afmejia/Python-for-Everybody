ce673c1c7bbf8064b995efa9a2644cc6680a810f
Felipe <afmejia23@utp.edu.co>
2017-12-29 10:44:44 +0200

    scrape the web

--- Course3/urljpeg.py
+++ Course3/urljpeg.py
@@ -5,7 +5,7 @@ HOST = 'data.pr4e.org'
 PORT = 80
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect((HOST, PORT))
-mysock.sendall(b'GET http://data.pr4e.org/cover3.jpg HTTP/1.0\n\n')
+mysock.sendall(b'GET http://data.pr4e.org/cover3.jpg HTTP/1.0\r\n\r\n')
 count = 0
 picture = b""
 
@@ -22,4 +22,10 @@ mysock.close()
 # Look for the end of the header (2 CRLF)
 pos = picture.find(b"\r\n\r\n")
 print('Header length', pos)
-print(picture[:pos].decode)
+print(picture[:pos].decode())
+
+# Skip past the header and save the picture data
+picture = picture[pos+4:]
+fhand = open("stuff.jpg", "wb")
+fhand.write(picture)
+fhand.close()
