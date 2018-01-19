7c0b54b2be4a1b27c67dfbdb0a2a1e499988ae14
Felipe <afmejia23@utp.edu.co>
2018-01-18 23:30:07 -0500

    Extracting XML

--- Course3/xml3.py
+++ Course3/xml3.py
@@ -1,8 +1,21 @@
 import urllib.request, urllib.parse, urllib.error
 import xml.etree.ElementTree as ET
 
-serviceurl = input('Enter location: ')
-try:
+serviceurl = input('Enter URL: ')
+#serviceurl = 'http://py4e-data.dr-chuck.net/comments_42.xml'
 
+try:
+    print("Retrieving", serviceurl)
+    fil = urllib.request.urlopen(serviceurl)
+    data = fil.read()
+    print('Retrieved', len(data), 'characters')
+    tree = ET.fromstring(data)
+    lst = tree.findall('comments/comment')
+    print('Comments: ', len(lst))
+    total = 0
+    for item in lst:
+        num = item.find('count').text
+        total += int(num)
+    print('Total:', total)
 except:
     print("Huston, we've got a problem")
